e92d095b9fd56e29fd78b765c947a6aa30db2b03
Pavel Maiboroda <successbloger1@gmail.com>
2021-01-04 12:15:54 +0200

    Pagination added

--- scraping/views.py
+++ scraping/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
 from .forms import SearchForm
@@ -8,7 +9,7 @@ def home_view(request):
     city = request.GET.get('city')
     language = request.GET.get('language')
 
-    query_set = []
+    page_obj = None
     if city or language:
         _filters = {}
         if city:
@@ -17,8 +18,14 @@ def home_view(request):
             _filters['language__slug'] = language
         query_set = Vacancy.objects.filter(**_filters)
 
+        paginator = Paginator(query_set, 10)  # Show 10 contacts per page.
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
     context = {
-        'object_list': query_set,
+        'city': city,
+        'language': language,
+        'object_list': page_obj,
         'form': SearchForm(),
     }
     return render(request, 'scraping/home.html', context)
