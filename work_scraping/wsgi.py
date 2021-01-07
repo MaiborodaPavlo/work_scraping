5d0b8ff0456774fd60eb0b68d2111febea4a9ea6
Pavel Maiboroda <successbloger1@gmail.com>
2021-01-07 18:09:27 +0200

    Celery settings

--- work_scraping/wsgi.py
+++ work_scraping/wsgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+import djcelery
+djcelery.setup_loader()
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'work_scraping.settings')
 
 application = get_wsgi_application()
